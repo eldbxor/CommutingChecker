4b3af987268719430223c63c1a0cd0dd34cb17aa
Awesometic <secugyu@gmail.com>
2016-08-23 12:31:54 +0900

    calibration: add threshold adjustment fragment

--- app/src/main/java/com/example/taek/commutingchecker/ui/CalibrationFragment.java
+++ app/src/main/java/com/example/taek/commutingchecker/ui/CalibrationFragment.java
@@ -1,22 +1,36 @@
 package com.example.taek.commutingchecker.ui;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.taek.commutingchecker.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Awesometic on 2016-08-22.
  */
 public class CalibrationFragment extends Fragment {
-    
+
     private View rootView;
+
+    Button btnCalibrationStart;
+
+    private Timer timer;
+    private TextView timerText;
+    private int timerSecond = 0;
+    private final Handler timerHandler = new Handler();
 
     public static CalibrationFragment newInstance() {
         CalibrationFragment fragment = new CalibrationFragment();
@@ -35,13 +49,41 @@ public class CalibrationFragment extends Fragment {
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_calibration, container, false);
 
-        Button calibration = (Button) rootView.findViewById(R.id.btn_calibration_start);
-        calibration.setOnClickListener(new View.OnClickListener() {
+        btnCalibrationStart = (Button) rootView.findViewById(R.id.btn_calibration_start);
+
+        btnCalibrationStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent("android.intent.action.CALIBRATION_SERVICE");
-                intent.setData(Uri.parse("Calibration:"));
-                getActivity().sendBroadcast(intent);
+                switch (btnCalibrationStart.getText().toString()) {
+
+                    case "START":
+                        // Calibration start code here
+
+                        timerStart();
+                        btnCalibrationStart.setText("RESET");
+
+                        break;
+
+                    case "RESET":
+                        // Calibration cancel code here
+
+                        timerStop();
+                        btnCalibrationStart.setText("START");
+
+                        break;
+
+                    case "NEXT":
+                        ThresholdAdjustmentFragment thresholdAdjustmentFragment = new ThresholdAdjustmentFragment();
+                        FragmentManager fragmentManager = getFragmentManager();
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.content_frame, thresholdAdjustmentFragment);
+                        fragmentTransaction.commit();
+
+                        break;
+
+                    default:
+                        break;
+                }
             }
         });
 
@@ -51,5 +93,40 @@ public class CalibrationFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+    }
+
+    private void timerStart() {
+        timerText = (TextView) rootView.findViewById(R.id.timer_text);
+        timerSecond = 0;
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                timerTextUpdate();
+                timerSecond++;
+            }
+        }, 0, 1000);
+    }
+
+    private void timerStop() {
+        timer.cancel();
+        timer = null;
+    }
+
+    private void timerTextUpdate() {
+        Runnable updater = new Runnable() {
+            @Override
+            public void run() {
+                timerText.setText(timerSecond + " 초");
+
+                if (timerSecond == 30) {
+                    timerStop();
+
+                    btnCalibrationStart.setText("NEXT");
+                }
+            }
+        };
+        timerHandler.post(updater);
     }
 }
